2f0acda8432cf435fbf47363175614998c6b2772
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2019-10-27 07:17:17 +0000

    Echo source commands into .bashrc using installer

--- .bashrc
+++
@@ -1,3 +0,0 @@
-[ -f bash/aliases.sh ] && source bash/aliases.sh
-[ -f bash/default.sh ] && source bash/default.sh
-[ -f bash/prompt.sh ] && source bash/prompt.sh
--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
-# overwrite current bashrc with one line that sources the custom one
-echo "[ -f ~/dotfiles/.bashrc ] && source ~/dotfiles/.bashrc" > ~/.bashrc
-
 # create an inputrc
 [ -f ~/dotfiles/.inputrc ] && cat ~/dotfiles/.inputrc > ~/.inputrc
+
+# source the shell scripts in this dotfiles folder
+echo "[ -f dotfiles/aliases.sh ] && source dotfiles/aliases.sh" > ~/.bashrc
+echo "[ -f dotfiles/default.sh ] && source dotfiles/default.sh" >> ~/.bashrc
+echo "[ -f dotfiles/prompt.sh ] && source dotfiles/prompt.sh" >> ~/.bashrc
