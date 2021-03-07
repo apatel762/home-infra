cda27632be1176cb836011de2b639bf57321634c
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2021-03-07 15:56:47 +0000

    add vim setup including vim-plug install

--- run.sh
+++ run.sh
@@ -13,16 +13,11 @@ require() {
         || oops "you do not have '$1' installed; needed for: $2"
 }
 
-log() {
-    echo "INFO - $1"
-}
-
 require ansible
 
 # gets the directory of this script
 ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-log "variables:"
 # use `set -x to log all commands from here onwards`
 set -x
 HOSTS="$ROOTDIR/hosts.ini"
