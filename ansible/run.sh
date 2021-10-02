d1f7ba4133eff9e9e8443ac7541b7cb952b0fec8
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2021-10-02 10:43:02 +0100

    add a bash script for generating ids to local bin

--- ansible/run.sh
+++ ansible/run.sh
@@ -18,12 +18,26 @@ require ansible
 # gets the directory of this script
 ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# ensure that the vault password file exists
+VAULT_PASSWORD_FILE="$ROOTDIR/vault-password-file"
+if [[ ! -f "$VAULT_PASSWORD_FILE" ]]; then
+    echo "No password file exists. Do the following:"
+    echo ""
+    echo "  $EDITOR $VAULT_PASSWORD_FILE"
+    echo "  chmod u=r,go-rwx $VAULT_PASSWORD_FILE"
+    echo ""
+    oops "Please create $VAULT_PASSWORD_FILE with strict permissions and re-run this script."
+fi
+
 # use `set -x` to log all commands from here onwards
 set -x
 HOSTS="$ROOTDIR/hosts.ini"
 PLAYBOOK="$ROOTDIR/playbook.yml"
 
 # runs Ansible playbook using our user.
-ansible-playbook -i "$HOSTS" "$PLAYBOOK" --ask-become-pass
+ansible-playbook \
+    --vault-password-file "$VAULT_PASSWORD_FILE" \
+    --inventory "$HOSTS" "$PLAYBOOK" \
+    --ask-become-pass
 
 exit 0
