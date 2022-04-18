3f51cc3ca87939a52ee417d7d2b0099072118d84
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2022-04-18 15:54:56 +0100

    ensure that the Neovim AppImage is extracted
    now, the Neovim AppImage will be extracted by one of the bash init
    scripts when you start an interactive terminal session and a symlink to
    the squashfs nvim binary will be added to the local bin so that you can
    use nvim in toolboxes the same way as you would on the host fs
    
    closes gh-46

--- workstation/dotfiles/dots/bash/.bashrc.d/03-EDITOR.sh
+++ workstation/dotfiles/dots/bash/.bashrc.d/03-EDITOR.sh
@@ -1,3 +1,36 @@
+function _ensure_neovim_appimage_extracted() {
+	local LOCAL_BIN
+	LOCAL_BIN="$HOME/.local/bin"
+
+	local EXPECTED_NEOVIM_BINARY
+	EXPECTED_NEOVIM_BINARY="$LOCAL_BIN/nvim" 
+
+	# if the symlink is broken, ensure that the target is removed
+	# ...and remove the squashfs root while we're at it (if it's there)
+	if [ ! -e "$EXPECTED_NEOVIM_BINARY" ] ; then
+		rm -f "$EXPECTED_NEOVIM_BINARY" 
+		rm -rf "$LOCAL_BIN/nvim-root"
+	fi 
+
+	if ! command -v nvim &>/dev/null \
+		&& [ -f "$HOME/.local/bin/nvim.AppImage" ]; then
+		# if we are in here, we have the AppImage in the local bin and we
+		# DON'T have the plain Neovim binary, so here, we need to extract
+		# it from the AppImage and create a symlink
+		(
+			echo "Creating physical nvim.AppImage alias"
+			cd "$HOME/.local/bin/" 
+			./nvim.AppImage --appimage-extract &>/dev/null
+
+			# rename the fs root folder
+			mv squashfs-root nvim-root
+
+			# create symlink
+			ln -s nvim-root/usr/bin/nvim nvim
+		)
+	fi
+}
+
 # if we've got 'nvim' then prioritise that over the other stuff
 if command -v nvim &>/dev/null; then
 	export VISUAL=nvim
@@ -5,6 +38,13 @@ if command -v nvim &>/dev/null; then
 	alias vi='vim'
 	alias vim='nvim'
 	[[ $(type -t nvim) == "alias" ]] && unalias nvim
+
+	# using the Neovim AppImage on my workstation, so if it's present, this
+	# init script will extract the AppImage and create the relevant
+	# symlinks to make it all work (even when inside of a toolbox, where
+	# you can't use FUSE mounts).
+	_ensure_neovim_appimage_extracted
+
 
 # if we've got 'vim', then use that over 'vi' to make things a little
 # bit easier to use
