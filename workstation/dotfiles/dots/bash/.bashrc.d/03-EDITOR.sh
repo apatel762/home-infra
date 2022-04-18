f6e57d501923117637e19a2c4989d81e832a51b4
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2022-04-18 15:19:47 +0100

    ensure consistency with aliases for vi, vim and nvim
    The aliases will prioritise the programs in the below order:
    
    1. Neovim
    2. Vim
    3. Vi
    
    so that the dotfiles will still work on a fresh system and automatically pick
    up Neovim after it's installed (and fall back to Vi or Vim if it's
    uninstalled).
    
    closes gh-48

--- workstation/dotfiles/dots/bash/.bashrc.d/03-EDITOR.sh
+++ workstation/dotfiles/dots/bash/.bashrc.d/03-EDITOR.sh
@@ -1,12 +1,27 @@
+# if we've got 'nvim' then prioritise that over the other stuff
 if command -v nvim &>/dev/null; then
-    export VISUAL=nvim
-    export EDITOR=nvim
-    alias vim='nvim'
-else
-    export VISUAL=vi
-    export EDITOR=vi
-fi
+	export VISUAL=nvim
+	export EDITOR=nvim
+	alias vi='vim'
+	alias vim='nvim'
+	[[ $(type -t nvim) == "alias" ]] && unalias nvim
 
-if ! command -v vim &>/dev/null; then
-    alias vim='vi'
+# if we've got 'vim', then use that over 'vi' to make things a little
+# bit easier to use
+elif command -v vim &>/dev/null; then
+	export VISUAL=vim
+	export EDITOR=vim
+	alias vi='vim'
+	[[ $(type -t vim) == "alias" ]] && unalias vim
+	alias nvim='vim'
+
+# we definitely should have 'vi' installed at the very least, so use
+# that in the aliases...
+else
+	export VISUAL=vi
+	export EDITOR=vi
+	[[ $(type -t vi) == "alias" ]] && unalias vi
+	alias vim='vi'
+	alias nvim='vi'
+
 fi
