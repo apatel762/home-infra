f9d561b4776eb6c20799aef6c3feedf633eba291
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2022-04-18 12:59:03 +0100

    unset bat as MANPAGER
    running into weird formatting issues when I look at man pages so
    removing this for now

--- workstation/dotfiles/dots/bash/.bashrc.d/04-MANPAGER.sh
+++
@@ -1,7 +0,0 @@
-# I want to use `bat` as the manpager if it's installed so that I get
-# syntax highlighting on manpages. Also, since it uses the same keys as
-# less, there's no reason not to use it if you have it.
-
-if command -v bat &>/dev/null; then
-    export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-fi
